884777e98ec8ddfe68fc9e7442bc6ac582f792d5
aylong-git <aylong2004@gmail.com>
2026-05-07 03:18:13 +0800

    Clean Initial C++ Build

---
+++ lib/cmake/brainflow/brainflowTargets-release.cmake
@@ -0,0 +1,129 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "brainflow::GanglionLib" for configuration "Release"
+set_property(TARGET brainflow::GanglionLib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::GanglionLib PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/GanglionLib.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/GanglionLib.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::GanglionLib )
+list(APPEND _cmake_import_check_files_for_brainflow::GanglionLib "${_IMPORT_PREFIX}/lib/GanglionLib.lib" "${_IMPORT_PREFIX}/lib/GanglionLib.dll" )
+
+# Import target "brainflow::MuseLib" for configuration "Release"
+set_property(TARGET brainflow::MuseLib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::MuseLib PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/MuseLib.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/MuseLib.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::MuseLib )
+list(APPEND _cmake_import_check_files_for_brainflow::MuseLib "${_IMPORT_PREFIX}/lib/MuseLib.lib" "${_IMPORT_PREFIX}/lib/MuseLib.dll" )
+
+# Import target "brainflow::BrainBitLib" for configuration "Release"
+set_property(TARGET brainflow::BrainBitLib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::BrainBitLib PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/BrainBitLib.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/BrainBitLib.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::BrainBitLib )
+list(APPEND _cmake_import_check_files_for_brainflow::BrainBitLib "${_IMPORT_PREFIX}/lib/BrainBitLib.lib" "${_IMPORT_PREFIX}/lib/BrainBitLib.dll" )
+
+# Import target "brainflow::SynchroniLib" for configuration "Release"
+set_property(TARGET brainflow::SynchroniLib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::SynchroniLib PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/SynchroniLib.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/SynchroniLib.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::SynchroniLib )
+list(APPEND _cmake_import_check_files_for_brainflow::SynchroniLib "${_IMPORT_PREFIX}/lib/SynchroniLib.lib" "${_IMPORT_PREFIX}/lib/SynchroniLib.dll" )
+
+# Import target "brainflow::BrainFlowBluetooth" for configuration "Release"
+set_property(TARGET brainflow::BrainFlowBluetooth APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::BrainFlowBluetooth PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/BrainFlowBluetooth.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/BrainFlowBluetooth.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::BrainFlowBluetooth )
+list(APPEND _cmake_import_check_files_for_brainflow::BrainFlowBluetooth "${_IMPORT_PREFIX}/lib/BrainFlowBluetooth.lib" "${_IMPORT_PREFIX}/lib/BrainFlowBluetooth.dll" )
+
+# Import target "brainflow::BoardController" for configuration "Release"
+set_property(TARGET brainflow::BoardController APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::BoardController PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/BoardController.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/BoardController.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::BoardController )
+list(APPEND _cmake_import_check_files_for_brainflow::BoardController "${_IMPORT_PREFIX}/lib/BoardController.lib" "${_IMPORT_PREFIX}/lib/BoardController.dll" )
+
+# Import target "brainflow::DSPFilters" for configuration "Release"
+set_property(TARGET brainflow::DSPFilters APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::DSPFilters PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/DSPFilters.lib"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::DSPFilters )
+list(APPEND _cmake_import_check_files_for_brainflow::DSPFilters "${_IMPORT_PREFIX}/lib/DSPFilters.lib" )
+
+# Import target "brainflow::WaveLib" for configuration "Release"
+set_property(TARGET brainflow::WaveLib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::WaveLib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/WaveLib.lib"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::WaveLib )
+list(APPEND _cmake_import_check_files_for_brainflow::WaveLib "${_IMPORT_PREFIX}/lib/WaveLib.lib" )
+
+# Import target "brainflow::kissfft-double" for configuration "Release"
+set_property(TARGET brainflow::kissfft-double APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::kissfft-double PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/kissfft-double.lib"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::kissfft-double )
+list(APPEND _cmake_import_check_files_for_brainflow::kissfft-double "${_IMPORT_PREFIX}/lib/kissfft-double.lib" )
+
+# Import target "brainflow::DataHandler" for configuration "Release"
+set_property(TARGET brainflow::DataHandler APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::DataHandler PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/DataHandler.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/DataHandler.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::DataHandler )
+list(APPEND _cmake_import_check_files_for_brainflow::DataHandler "${_IMPORT_PREFIX}/lib/DataHandler.lib" "${_IMPORT_PREFIX}/lib/DataHandler.dll" )
+
+# Import target "brainflow::MLModule" for configuration "Release"
+set_property(TARGET brainflow::MLModule APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::MLModule PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/MLModule.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/MLModule.dll"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::MLModule )
+list(APPEND _cmake_import_check_files_for_brainflow::MLModule "${_IMPORT_PREFIX}/lib/MLModule.lib" "${_IMPORT_PREFIX}/lib/MLModule.dll" )
+
+# Import target "brainflow::Brainflow" for configuration "Release"
+set_property(TARGET brainflow::Brainflow APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(brainflow::Brainflow PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/Brainflow.lib"
+  )
+
+list(APPEND _cmake_import_check_targets brainflow::Brainflow )
+list(APPEND _cmake_import_check_files_for_brainflow::Brainflow "${_IMPORT_PREFIX}/lib/Brainflow.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
